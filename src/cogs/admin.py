5aff6ec80b6b9aaa4ff103669922afafa47eb863
Kilin <jack233924@gmail.com>
2024-01-24 02:09:43 +0800

    Changing some structure.

--- src/cogs/admin.py
+++ src/cogs/admin.py
@@ -1,35 +1,38 @@
-import asyncio
+from __future__ import annotations
+
+import os
+from typing import TYPE_CHECKING, Literal, Optional
+import importlib
+
 import discord
 from discord.ext import commands
-from typing import Optional, Literal
-import os
 
-# Add parent directory to path
-cog_dir = os.path.dirname(os.path.realpath(__file__))
+from cogs import EXTENSIONS
+
+if TYPE_CHECKING:
+    from main import FactoryBot
 
 class Admin(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: FactoryBot):
         self.bot = bot
         self.description = '''A cog for admin commands. Including reload, sync, etc.'''
 
     @commands.command(name="reload")
     @commands.is_owner()
     async def reload(self, ctx: commands.Context):
-        for filename in os.listdir(cog_dir):
-            if not filename.endswith(".py"):
-                continue
+        for extension in EXTENSIONS:
             try:
-                await self.bot.reload_extension(f"cogs.{filename[:-3]}")
-                await ctx.send(f"{filename[:-3]} is reloaded.")
+                await self.bot.reload_extension(extension)
+                await ctx.send(f"`{extension}` is reloaded.")
             except commands.ExtensionNotLoaded:
-                await ctx.send(f"{filename[:-3]} is not loaded.")
-                await self.bot.load_extension(f"cogs.{filename[:-3]}")
+                await ctx.send(f"`{extension}` is not loaded.")
+                await self.bot.load_extension(extension)
             except commands.ExtensionNotFound:
-                await ctx.send(f"{filename[:-3]} is not found.")
+                await ctx.send(f"`{extension}` is not found.")
             except commands.NoEntryPointError:
-                await ctx.send(f"{filename[:-3]} has no setup function.")
+                await ctx.send(f"`{extension}` has no setup function.")
             except commands.ExtensionFailed:
-                await ctx.send(f"{filename[:-3]} had an execution error.")
+                await ctx.send(f"`{extension}` had an execution error.")
         loaded_cogs = [f"\r- **{name}**:\r {cog.description}" for name, cog in self.bot.cogs.items()]
         loaded_cogs_str = "".join(loaded_cogs)
         await ctx.send(f"# Current loaded cogs:{loaded_cogs_str}")
@@ -52,7 +55,7 @@ class Admin(commands.Cog):
             elif spec == "^":           # This command will remove all guild commands from the CommandTree and syncs, which effectively removes all commands from the guild.
                 ctx.bot.tree.clear_commands(guild=ctx.guild)
                 await ctx.bot.tree.sync(guild=ctx.guild)
-                synced = []             # This command will sync the 3 guild ids we passed: 123, 456 and 789. Only their guilds and guild-bound commands.
+                synced = []             
             else:                       # Take all global commands and send them to Discord.
                 synced = await ctx.bot.tree.sync()
 
@@ -63,7 +66,7 @@ class Admin(commands.Cog):
             return
 
         ret = 0
-        for guild in guilds:
+        for guild in guilds:    # This command will sync the 3 guild ids we passed: 123, 456 and 789. Only their guilds and guild-bound commands.
             try:
                 await ctx.bot.tree.sync(guild=guild)
             except discord.HTTPException:
--- src/main.py
+++ src/main.py
@@ -4,19 +4,18 @@ import logging.handlers
 import os
 import sys
 from typing import Optional
+import configparser
 
 import asyncpg
 import discord
 from aiohttp import ClientSession
 from discord.ext import commands
 
-from utils.database import FactoryDB
-from utils.config import Config
+from cogs import EXTENSIONS
 
 # Add parent directory to path
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -32,29 +31,84 @@ class FactoryBot(commands.Bot):
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
-        self.db = FactoryDB(db_pool)
+        self.db = db_pool
         self.web_client = web_client
         self.testing_guild_id = testing_guild_id
 
     async def setup_hook(self):
         # Load cogs
-        for filename in os.listdir(current_dir + "/cogs"):
-            if filename.endswith(".py"):
-                await self.load_extension(f"cogs.{filename[:-3]}")
+        for extension in EXTENSIONS:
+            await self.load_extension(extension)
 
     async def on_ready(self):
-        await self.db._db_setup()
+        await self._db_setup()
         print(f"We have logged in as {self.user}")
+        
+    async def _db_setup(self):
+        async with self.db.acquire() as connection:
+            # Check if the schema exists
+            schema_exists = await connection.fetchval("""
+                SELECT EXISTS (
+                    SELECT 1
+                    FROM pg_catalog.pg_namespace
+                    WHERE nspname = 'factorybot'
+                );
+            """)
+
+            # Create the schema if it does not exist
+            if not schema_exists:
+                await connection.execute("CREATE SCHEMA factorybot;")
+                print("Schema 'factorybot' created.")
+            else:
+                print("Schema 'factorybot' already exists.")
+        
+        # Check if the thread tables exist in the schema. If not, create them.
+        async with self.db.acquire() as connection:
+            # Create the table
+            await connection.execute("""
+                CREATE TABLE IF NOT EXISTS factorybot.threads (
+                    user_id bigint NOT NULL,
+                    thread_id bigint,
+                    guild_id bigint,
+                    "time" date,
+                    PRIMARY KEY (user_id)
+                );
+
+                ALTER TABLE factorybot.threads
+                OWNER TO chilling;
+            """)
+            print("Table 'threads' checked/created in schema 'factorybot'.")
+            
+        # Check if the profile tables exist in the schema. If not, create them.
+        async with self.db.acquire() as connection:
+            # Create the table
+            await connection.execute("""
+                CREATE TABLE factorybot.profiles
+                (
+                    user_id bigint,
+                    name character(20),
+                    description character(100),
+                    instruction character(5000),
+                    model_id integer,
+                    params json,
+                    PRIMARY KEY (user_id)
+                );
+
+                ALTER TABLE IF EXISTS factorybot.profiles
+                    OWNER to chilling;
+            """)
+            print("Table 'profiles' checked/created in schema 'factorybot'.")
 
 
 async def main():
-    config = Config(parent_dir + "/config.ini")
+    config = configparser.ConfigParser()
+    config.read(parent_dir + "/config.ini")
     # Logging
     logger = logging.getLogger("discord")
     logger.setLevel(logging.INFO)
 
     handler = logging.handlers.RotatingFileHandler(
-        filename="discord.log",
+        filename=parent_dir + "discord.log",
         encoding="utf-8",
         maxBytes=32 * 1024 * 1024,  # 32 MiB
         backupCount=5,  # Rotate through 5 files
@@ -68,11 +122,11 @@ async def main():
 
     # Bot
     async with ClientSession() as web_client, asyncpg.create_pool(
-        user=config.postgresql_user(),
-        password=config.postgresql_password(),
-        host=config.postgresql_host(),
-        port=config.postgresql_port(),
-        database=config.postgresql_database(),
+        user=config["POSTGRESQL"]["user"],
+        password=config["POSTGRESQL"]["password"],
+        host=config["POSTGRESQL"]["host"],
+        port=config["POSTGRESQL"]["port"],
+        database=config["POSTGRESQL"]["database"],
         min_size=3,
         command_timeout=30,
     ) as pool:
@@ -82,7 +136,7 @@ async def main():
             db_pool=pool,
             web_client=web_client,
         ) as bot:
-            await bot.start(config.bot_token())
+            await bot.start(config["BOT"]["token"])
 
 
 asyncio.run(main())
